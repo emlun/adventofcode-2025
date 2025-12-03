c3905f07575ff4d19c9cfb107ab472ee4445574d
Emil Lundberg <emil@emlun.se>
2025-12-03 09:13:48 +0100

    Solve day 3b

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -18,17 +18,28 @@ use std::cmp::Reverse;
 
 use crate::common::Solution;
 
-fn solve_a(ranges: &[Vec<u32>]) -> u32 {
-    ranges
+fn select_battery(bank: &[u32], prev_batteries: &[usize], num_batteries: usize) -> usize {
+    let (ri, _) = bank[..bank.len() - (num_batteries - 1 - prev_batteries.len())]
         .iter()
-        .map(|batteries| {
-            let (li, l) = batteries[..batteries.len() - 1]
-                .iter()
-                .enumerate()
-                .max_by_key(|(i, b)| (**b, Reverse(*i)))
-                .unwrap();
-            let r = batteries.iter().skip(li + 1).max().unwrap();
-            l * 10 + r
+        .enumerate()
+        .skip(prev_batteries.last().copied().map(|i| i + 1).unwrap_or(0))
+        .max_by_key(|(bi, b)| (**b, Reverse(*bi)))
+        .unwrap();
+    ri
+}
+
+fn solve_b(banks: &[Vec<u32>], num_batteries: usize) -> u64 {
+    banks
+        .iter()
+        .map(|bank| {
+            let selected_bis = (0..num_batteries).fold(vec![], |mut selected_bis, _| {
+                let bi = select_battery(bank, &selected_bis, num_batteries);
+                selected_bis.push(bi);
+                selected_bis
+            });
+            selected_bis
+                .into_iter()
+                .fold(0_u64, |acc, bi| acc * 10 + u64::from(bank[bi]))
         })
         .sum()
 }
@@ -41,5 +52,8 @@ pub fn solve(lines: &[String]) -> Solution {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
 
-    (solve_a(&banks).to_string(), "".to_string())
+    (
+        solve_b(&banks, 2).to_string(),
+        solve_b(&banks, 12).to_string(),
+    )
 }
