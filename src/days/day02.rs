2af3ff1bfd18a7be9518fdf274e9fc2615aabe5b
Emil Lundberg <emil@emlun.se>
2025-12-04 08:20:31 +0100

    Solve day 2b

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -14,16 +14,33 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::collections::HashSet;
+
 use crate::common::Solution;
 
-fn parse_split_middle(s: &str) -> (usize, usize) {
-    if s.len() == 1 {
-        let si = s.parse().unwrap();
-        (si, si)
+fn parse_split_n<const N: usize>(s: &str) -> Option<[usize; N]> {
+    if s.len() % N == 0 {
+        let l = s.len() / N;
+        let mut result = [0; N];
+        for i in 0..N {
+            result[i] = s[i * l..(i + 1) * l].parse().unwrap();
+        }
+        Some(result)
     } else {
-        let l = s.len() / 2;
-        let ll = s.len() % 2;
-        (s[0..(l + ll)].parse().unwrap(), s[l..].parse().unwrap())
+        None
+    }
+}
+
+fn parse_split(s: &str, n: usize) -> Option<Vec<usize>> {
+    if s.len() % n == 0 {
+        let l = s.len() / n;
+        Some(
+            (0..n)
+                .map(|i| s[i * l..(i + 1) * l].parse().unwrap())
+                .collect(),
+        )
+    } else {
+        None
     }
 }
 
@@ -34,8 +51,7 @@ fn solve_a(ranges: &[(&str, &str)]) -> usize {
             if ls.len() == rs.len() && ls.len() % 2 != 0 {
                 1_usize..=0_usize
             } else {
-                let min = if ls.len() % 2 == 0 {
-                    let (minl, minr) = parse_split_middle(ls);
+                let min = if let Some([minl, minr]) = parse_split_n(ls) {
                     if minr > minl {
                         minl + 1
                     } else {
@@ -44,8 +60,7 @@ fn solve_a(ranges: &[(&str, &str)]) -> usize {
                 } else {
                     10_usize.pow(u32::try_from(ls.len().div_ceil(2)).unwrap() - 1)
                 };
-                let max = if rs.len() % 2 == 0 {
-                    let (maxl, maxr) = parse_split_middle(rs);
+                let max = if let Some([maxl, maxr]) = parse_split_n(rs) {
                     if maxr < maxl {
                         maxl - 1
                     } else {
@@ -69,6 +84,58 @@ fn solve_a(ranges: &[(&str, &str)]) -> usize {
         .sum()
 }
 
+fn solve_b(ranges: &[(&str, &str)]) -> usize {
+    ranges
+        .iter()
+        .flat_map(|(ls, rs)| {
+            (2..=rs.len()).flat_map(move |repeats| {
+                if ls.len() == rs.len() && ls.len() % repeats != 0 {
+                    1_usize..=0_usize
+                } else {
+                    let min = if let Some(mins) = parse_split(ls, repeats) {
+                        mins.iter().rev().fold(
+                            0,
+                            |minr, minl| {
+                                if minr > *minl {
+                                    minl + 1
+                                } else {
+                                    *minl
+                                }
+                            },
+                        )
+                    } else {
+                        10_usize.pow(u32::try_from(ls.len().div_ceil(repeats)).unwrap() - 1)
+                    };
+                    let max = if let Some(maxs) = parse_split(rs, repeats) {
+                        maxs.iter().rev().fold(usize::MAX, |maxr, maxl| {
+                            if maxr < *maxl {
+                                maxl - 1
+                            } else {
+                                *maxl
+                            }
+                        })
+                    } else {
+                        10_usize.pow(u32::try_from(rs.len() / repeats).unwrap()) - 1
+                    };
+
+                    min..=max
+                }
+                .map(move |n| {
+                    let log = n.ilog10() + 1;
+                    let nn = (0..repeats).fold(0, |nn, i| {
+                        nn + 10_usize.pow(log * u32::try_from(i).unwrap()) * n
+                    });
+                    debug_assert!(nn >= ls.parse().unwrap());
+                    debug_assert!(nn <= rs.parse().unwrap());
+                    nn
+                })
+            })
+        })
+        .collect::<HashSet<usize>>()
+        .into_iter()
+        .sum()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let ranges: Vec<(&str, &str)> = lines
         .iter()
@@ -80,5 +147,5 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .collect();
 
-    (solve_a(&ranges).to_string(), "".to_string())
+    (solve_a(&ranges).to_string(), solve_b(&ranges).to_string())
 }
