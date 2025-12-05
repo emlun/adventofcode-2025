eb95b37bc3d9319154791fddcf8d997fa1841d46
Emil Lundberg <emil@emlun.se>
2025-12-05 10:07:24 +0100

    Optimize day 5

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -28,29 +28,21 @@ where
 {
     if a.start() > b.start() {
         merge(b, a)
-    } else if a.start() <= b.start() && a.end() >= b.end() {
-        (a, None)
     } else if b.start() > a.end() {
         (a, Some(b))
     } else {
-        ((*a.start())..=(*b.end()), None)
+        ((*a.start())..=(*std::cmp::max(a.end(), b.end())), None)
     }
 }
 
-fn solve_a(fresh: &[RangeInclusive<usize>], ids: &[usize]) -> usize {
-    ids.iter()
-        .filter(|id| fresh.iter().any(|range| range.contains(id)))
-        .count()
-}
-
-fn solve_b(mut fresh: Vec<RangeInclusive<usize>>) -> usize {
-    fresh.sort_by_key(|range| *range.start());
-    let l = fresh.len();
-    let mut it = fresh.into_iter();
-    let first = it.next().unwrap();
-    let (mut merged, last) = it.fold(
-        (Vec::with_capacity(l), first),
-        |(mut result, last), next| {
+fn merge_all<T>(ranges: Vec<RangeInclusive<T>>) -> Vec<RangeInclusive<T>>
+where
+    T: Copy,
+    T: Ord,
+{
+    let mut it = ranges.into_iter();
+    if let Some(first) = it.next() {
+        let (mut merged, last) = it.fold((Vec::new(), first), |(mut result, last), next| {
             let (a, b) = merge(last, next);
             if let Some(b) = b {
                 result.push(a);
@@ -58,14 +50,29 @@ fn solve_b(mut fresh: Vec<RangeInclusive<usize>>) -> usize {
             } else {
                 (result, a)
             }
-        },
-    );
-    merged.push(last);
-    merged.into_iter().map(|range| range.count()).sum()
+        });
+        merged.push(last);
+        merged
+    } else {
+        Vec::new()
+    }
+}
+
+fn solve_a(fresh: &[RangeInclusive<usize>], ids: &[usize]) -> usize {
+    ids.iter()
+        .filter(|id| {
+            let max = fresh.partition_point(|range| range.start() < id);
+            fresh[..max].iter().rev().any(|range| range.contains(id))
+        })
+        .count()
+}
+
+fn solve_b(fresh: Vec<RangeInclusive<usize>>) -> usize {
+    fresh.into_iter().map(|range| range.count()).sum()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let (fresh, ids, _): (Vec<RangeInclusive<usize>>, Vec<usize>, bool) = lines.iter().fold(
+    let (mut fresh, ids, _): (Vec<RangeInclusive<usize>>, Vec<usize>, bool) = lines.iter().fold(
         (Vec::new(), Vec::new(), false),
         |(mut fresh, mut ids, parse_ids), line| {
             if line.is_empty() {
@@ -80,6 +87,8 @@ pub fn solve(lines: &[String]) -> Solution {
             }
         },
     );
+    fresh.sort_by_key(|range| *range.start());
+    fresh = merge_all(fresh);
     (
         solve_a(&fresh, &ids).to_string(),
         solve_b(fresh).to_string(),
