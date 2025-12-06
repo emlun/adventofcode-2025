74e742ea2d044581895dddc2295a5d5b215d3927
Emil Lundberg <emil@emlun.se>
2025-12-06 10:45:13 +0100

    Solve day 6b

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -21,6 +21,12 @@ struct Problem {
     mul: bool,
 }
 
+#[derive(Debug)]
+struct ProblemBParsing {
+    operands: Vec<Vec<(usize, u64)>>,
+    mul: bool,
+}
+
 fn solve_a(problems: &[Problem]) -> u64 {
     problems
         .iter()
@@ -32,6 +38,90 @@ fn solve_a(problems: &[Problem]) -> u64 {
             }
         })
         .sum()
+}
+
+fn solve_b(lines: &[String]) -> u64 {
+    let problems: Vec<ProblemBParsing> = lines
+        .iter()
+        .filter(|line| !line.is_empty())
+        .enumerate()
+        .fold(Vec::new(), |problems: Vec<ProblemBParsing>, (r, line)| {
+            let (problems, _, _) = line.chars().enumerate().fold(
+                (problems, 0, true),
+                |(mut problems, problem_i, last_was_whitespace): (
+                    Vec<ProblemBParsing>,
+                    usize,
+                    bool,
+                ),
+                 (c, ch)| {
+                    if ch == ' ' {
+                        if last_was_whitespace {
+                            (problems, problem_i, true)
+                        } else {
+                            (problems, problem_i + 1, true)
+                        }
+                    } else {
+                        if problems.len() <= problem_i {
+                            problems.resize_with(problem_i + 1, || ProblemBParsing {
+                                operands: Vec::with_capacity(4),
+                                mul: false,
+                            });
+                        }
+                        match ch {
+                            '+' | '*' => {
+                                problems[problem_i].mul = ch == '*';
+                                (problems, problem_i, false)
+                            }
+                            digit => {
+                                problems[problem_i].operands.resize_with(r + 1, Vec::new);
+                                problems[problem_i].operands[r]
+                                    .push((c, digit.to_digit(10).unwrap().into()));
+                                (problems, problem_i, false)
+                            }
+                        }
+                    }
+                },
+            );
+            problems
+        });
+    solve_a(
+        &problems
+            .into_iter()
+            .map(|problem| {
+                let min_c = problem
+                    .operands
+                    .iter()
+                    .flat_map(|op| op.iter())
+                    .map(|(c, _)| c)
+                    .min()
+                    .copied()
+                    .unwrap();
+                let max_c = problem
+                    .operands
+                    .iter()
+                    .flat_map(|op| op.iter())
+                    .map(|(c, _)| c)
+                    .max()
+                    .copied()
+                    .unwrap();
+                Problem {
+                    operands: problem.operands.into_iter().fold(
+                        vec![0; max_c + 1 - min_c],
+                        |operands, digits| {
+                            digits
+                                .into_iter()
+                                .fold(operands, |mut operands, (c, digit)| {
+                                    let cc = c - min_c;
+                                    operands[cc] = operands[cc] * 10 + digit;
+                                    operands
+                                })
+                        },
+                    ),
+                    mul: problem.mul,
+                }
+            })
+            .collect::<Vec<_>>(),
+    )
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -57,5 +147,5 @@ pub fn solve(lines: &[String]) -> Solution {
                     problems
                 })
         });
-    (solve_a(&problems).to_string(), "".to_string())
+    (solve_a(&problems).to_string(), solve_b(lines).to_string())
 }
