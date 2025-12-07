0b1b52c16e4df6d87da5bc3d6e936b39906237a6
Emil Lundberg <emil@emlun.se>
2025-12-07 08:50:17 +0100

    Use Vec instead of HashMap in day 7

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -14,39 +14,33 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::collections::HashMap;
-
 use crate::common::Solution;
 
 pub fn solve(lines: &[String]) -> Solution {
-    let start = lines.iter().find_map(|line| line.find('S')).unwrap();
-    let paths = {
-        let mut paths = HashMap::with_capacity(lines[0].len());
-        paths.insert(start, 1);
-        paths
-    };
-    let (paths, sol_a): (HashMap<usize, u64>, usize) = lines
+    let mut it = lines
         .iter()
         .map(|line| line.trim())
-        .filter(|line| !line.is_empty())
-        .skip(1)
-        .fold((paths, 0), |(paths, splits), line| {
-            line.chars()
-                .enumerate()
-                .fold((paths, splits), |(mut paths, splits), (i, ch)| {
-                    if ch == '^' {
-                        if let Some(num_paths) = paths.remove(&i) {
-                            *paths.entry(i - 1).or_insert(0) += num_paths;
-                            *paths.entry(i + 1).or_insert(0) += num_paths;
-                            (paths, splits + 1)
-                        } else {
-                            (paths, splits)
-                        }
-                    } else {
-                        (paths, splits)
-                    }
-                })
-        });
-    let sol_b: u64 = paths.values().sum();
+        .filter(|line| !line.is_empty());
+    let paths = it
+        .next()
+        .unwrap()
+        .chars()
+        .map(|ch| if ch == 'S' { 1 } else { 0 })
+        .collect();
+    let (paths, sol_a): (Vec<u64>, usize) = it.fold((paths, 0), |(paths, splits), line| {
+        line.chars()
+            .enumerate()
+            .fold((paths, splits), |(mut paths, splits), (i, ch)| {
+                if ch == '^' && paths[i] > 0 {
+                    paths[i - 1] += paths[i];
+                    paths[i + 1] += paths[i];
+                    paths[i] = 0;
+                    (paths, splits + 1)
+                } else {
+                    (paths, splits)
+                }
+            })
+    });
+    let sol_b: u64 = paths.into_iter().sum();
     (sol_a.to_string(), sol_b.to_string())
 }
